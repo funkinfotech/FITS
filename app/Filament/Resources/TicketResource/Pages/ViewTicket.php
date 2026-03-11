4365915b0427e43880e75714d31bf7b7153d6710
Funk <sweatybynature@gmail.com>
2026-03-10 23:05:52 -0400

    reverting back a few

--- app/Filament/Resources/TicketResource/Pages/ViewTicket.php
+++ app/Filament/Resources/TicketResource/Pages/ViewTicket.php
@@ -2,45 +2,10 @@
 
 namespace App\Filament\Resources\TicketResource\Pages;
 
-use App\Enums\TicketStatus;
 use App\Filament\Resources\TicketResource;
-use Filament\Actions;
-use Filament\Forms;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewTicket extends ViewRecord
 {
     protected static string $resource = TicketResource::class;
-
-    protected static string $view = 'filament.resources.ticket-resource.pages.view-ticket';
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\Action::make('addComment')
-                ->label('Add Comment')
-                ->icon('heroicon-o-chat-bubble-left-right')
-                ->form([
-                    Forms\Components\Textarea::make('content')
-                        ->label('Comment')
-                        ->required()
-                        ->rows(5)
-                        ->maxLength(5000),
-                ])
-                ->action(function (array $data): void {
-                    $this->record->comments()->create([
-                        'content' => $data['content'],
-                        'user_id' => auth()->id(),
-                    ]);
-
-                    if ($this->record->status === TicketStatus::Open) {
-                        $this->record->update([
-                            'status' => TicketStatus::InProgress,
-                        ]);
-                    }
-                }),
-
-            Actions\EditAction::make(),
-        ];
-    }
 }
--- resources/views/filament/resources/ticket-resource/pages/view-ticket.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-panels::page>
-    {{ $this->infolist }}
-
-    <div class="mt-8">
-        <x-filament::section heading="Conversation">
-            @include('filament.tickets.conversation-timeline', ['record' => $record])
-        </x-filament::section>
-    </div>
-</x-filament-panels::page>
